91837d020511108392c69f94145f6b37fa60293b
Shun Yamada <shantianshun42@gmail.com>
2026-08-01 01:59:47 +0900

    seitch文を学習

--- src/section_09/SwitchSyntax_05.java
+++ src/section_09/SwitchSyntax_05.java
@@ -2,6 +2,18 @@ package section_09;
 
 public class SwitchSyntax_05 {
     public static void main(String[]args){
+        int age = 26;
+        age = age / 10;
+        //System.out.println(age);
+        String kin = "";
 
+        kin = switch(age){
+            case 1 -> "1000円";
+            case 2 -> "2000円";
+            case 3,4 -> "3000円";
+            case 5 -> "4000円";
+            default -> "500円";
+        };
+        System.out.println(kin);
     }
 }

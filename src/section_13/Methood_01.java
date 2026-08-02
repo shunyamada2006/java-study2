81084a2e82e6b3b94c364991629954ab5ba2c5eb
Shun Yamada <shantianshun42@gmail.com>
2026-08-02 16:48:49 +0900

    メソッドを学習

---
+++ src/section_13/Methood_01.java
@@ -0,0 +1,12 @@
+package section_13;
+
+public class Methood_01 {
+    public static void main(String[]args){
+
+    }
+    public static void greeting(){
+        System.out.println("おはようございます");
+        System.out.println("昨日はよく眠れましたか？");
+        System.out.println("今日も頑張りましょう");
+    }
+}
---
+++ src/section_13/Methood_02.java
@@ -0,0 +1,18 @@
+package section_13;
+
+public class Methood_02 {
+    public static void main(String[]args){
+        int greeting =123;
+
+        greeting();
+
+        System.out.println(greeting);
+
+        greeting();
+    }
+    public static void greeting(){
+        System.out.println("おはようございます");
+        System.out.println("昨日はよく眠れましたか？");
+        System.out.println("今日も頑張りましょう");
+    }
+}

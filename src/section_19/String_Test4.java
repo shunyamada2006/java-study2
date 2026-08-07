02fda51370a5133fd76e3c81cfa4a4cb8dab497c
Shun Yamada <shantianshun42@gmail.com>
2026-08-08 01:32:35 +0900

    文字列を扱うクラスを学習

---
+++ src/section_19/String_Test4.java
@@ -0,0 +1,10 @@
+package section_19;
+
+public class String_Test4 {
+    public static void main(String[]args){
+        String moji1="あいうえおかきくけっこ";
+
+        System.out.println(moji1.substring(3));
+        System.out.println(moji1.substring(3,5));
+    }
+}
